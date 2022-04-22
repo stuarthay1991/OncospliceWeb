93930ee6f1f6692325107e7fde3e773e691b2db9
stuarthay1991 <haysb91@gmail.com>
2022-04-22 19:03:04 -0400

    Removed redundant file route.js, using target.js instead

--- targeturl.js
+++ targeturl.js
@@ -1,8 +1,18 @@
-var localurl = "/material-app";
+console.log('process.argv', process.argv);
+
+if(process.argv[2] == "local")
+{
+	const targeturl = "/material-app";
+}
+else
+{
+	const targeturl = "/ICGS/Oncosplice/testing";
+}
+/*var localurl = "/material-app";
 var serverurl = "/ICGS/Oncosplice/testing";
 var buildurl = "/ICGS/Oncosplice/build";
 var hoturl = "/ICGS/Oncosplice/hotload";
-const targeturl = serverurl;
+const targeturl = serverurl;*/
 
- 
+
 export default targeturl;
