d6e0369e28dd0c531675106ea747b4e806352550
Preeti <singhpreeti236@gmail.com>
2022-09-19 16:16:12 -0400

    Cbioportal update (#11)
    * partially working implementation for cbioportal linkout
    
    * linkout support functional; but full ui implementation needed
    
    * 6 of 9 cancers have full cbioportal support
    
    * Fixed HNSCC and COAD cancers, mapped the correct cancer IDs for each using table, added disabled button and tooltip css flags.
    
    * Removed dead+useless code and commented out fragments. Separated all cluster views from the View Pane, gave them and their child objects proper names and cleaned them up. Move all of these to the new 'plots' directory
    
    * Fixed minor bugs, deleted some useless printouts, basic cleanu[
    
    * Last functioning version before production
    
    * fixed gtex plot load, branched off into the plots folder.
    
    * Delete signature and signature access reworking implemented. Some refactoring on view panel plotting.
    
    * ExportView fixed.
    
    * No selection bug fixed
    
    * Cleaned up messy definitions
    
    * Set up new file for exonPlot, initiated branching off
    
    * setting up state functions
    
    * exonplot successfully branched off of view panel
    
    * Revisions added
    
    * changed download.js name
    
    * changed var delcarations to let in cbioportal.js
    
    * fixed remaining revisions
    
    * Changed panel width on build query panel, set up new component for selection boxes, but it is currently still material UI, need to change it to bootstrap. Will change that in the next commit
    
    * Implemented a single styled component for the build query selections, but this version is too blocky and I need to stick it in a few more places.
    
    * Implemented uniform selection component for all left hand combo boxes
    
    * Changed some css and margins, and implemented the react-bootstrap version of select.
    
    * More css changes, changed to bootstrap for the checkbox. However, doesn't look quite right, so need to fix.
    
    * Trying to use react-bootstrap checkbox drove me crazy, so I just scaled up the generic version instead. I think it looks ok, also fiddled around with margins
    
    * Bug fixed with interface, added more uniform font sizes.
    
    * updated gitignore
    
    * Making corrections that were mentioned in comments
    
    * Changed state obj to camel case
    
    * Deleted commented code, added more camel case replacements
    
    * tiny correction in mainpanel
    
    * changed postoncosig to listOfSelectedSignatures
    
    * isolated OKMAP_COLUMN_CLUSTERS to the plots directory
    
    * Isolated RPSI to a single file in the plots folder.
    
    * Cleaned up data table for events
    
    * Added plotpanel component to remove redundant code and provide better organization. Also removed some old unnecessary objects and cleaned up.
    
    * resizing fonts
    
    * view panel null submission fix
    
    * cryptic names like chicken and egg replaced
    
    * cryptic reference changed
    
    * cryptic reference mystring changed to querystring
    
    * old comments deleted
    
    * more minor corrections, mostly removing commented code and unused spaghetti code
    
    * clunky, but functioning replacement of the material-ui select component on ClientSelectedFilter. Trying to use spans and flexboxes instead of the material-ui grid system as well; this will be next on the agenda.
    
    * Less janky version. Next commit should do the trick.
    
    * Should be good now. Also messed around with the filter item---changed background color to black and added some padding. I don't think the black color looks great, but I don't really care for the way is was before either. Also will need to quickly scale down the label for the ClientSelectedFilter
    
    * renaming
    
    * git add . statement
    
    * cBioportal cancer study map linkup---still broken
    
    * cBioportal fix is getting way more complicated than it should, but I did make some progress here. It's at least retrieving the right name. Need to update the server side too
    
    * finally works
    
    * deleted old hardcoded map for cbioportal
    
    Co-authored-by: stuarthay1991 <haysb91@gmail.com>
    Co-authored-by: Preeti Singh <singhpreeti236.com>

--- src/components/cBioportalLinkout.js
+++ src/components/cBioportalLinkout.js
@@ -9,18 +9,6 @@ function goToCBio(uuid)
 	window.open(("https://www.cbioportal.org/comparison/survival?comparisonId=".concat(uuid)), "_blank");
 }
 
-const cancerCodeTranslate = {
-	"BRCA": "brca_tcga",
-	"BLCA": "blca_tcga",
-	"LGG": "lgg_tcga",
-	"LUAD": "luad_tcga",
-	"SKCM": "skcm_tcga",
-	"GBM": "gbm_tcga",
-	"HNSCC": "hnsc_tcga",
-	"COAD": "coadread_tcga",
-	"AML_Leucegene": null
-}
-
 //This function retrieves the sample names and groups from the parent component and formats them into the proper format for use in cBioportal.
 //After formatting, axios is used to send the information to a PHP file that uses a cURL command to retrieve the string needed to complete the cBioportal url.
 function sendSamplesRetrieveURL(props)
@@ -40,40 +28,52 @@ function sendSamplesRetrieveURL(props)
 			cBioportalJsonData.push(tempHoldingArray);
 		}
 	}
-	
-	let curlCommandJsonDataObject = {"groups": [], "origin": [studyID]};
-	let studyID = cancerCodeTranslate[props.cancer];
-
-	for(let i in cBioportalJsonData)
-	{
-		curlCommandJsonDataObject["groups"][i] = {};
-		curlCommandJsonDataObject["groups"][i]["name"] = props.label[i];
-		curlCommandJsonDataObject["groups"][i]["description"] = "";
-		curlCommandJsonDataObject["groups"][i]["studies"] = [];
-		curlCommandJsonDataObject["groups"][i]["studies"][0] = {};
-		curlCommandJsonDataObject["groups"][i]["studies"][0]["id"] = studyID;
-		curlCommandJsonDataObject["groups"][i]["studies"][0]["samples"] = cBioportalJsonData[i];
-		curlCommandJsonDataObject["groups"][i]["studies"][0]["patients"] = cBioportalJsonData[i];
-		curlCommandJsonDataObject["groups"][i]["origin"] = [];
-		curlCommandJsonDataObject["groups"][i]["origin"][0] = studyID;
-		curlCommandJsonDataObject["groups"][i]["uid"] = "62665b890934121b56df06b5".concat(i.toString());
-		curlCommandJsonDataObject["groups"][i]["isSharedGroup"] = false;
-		curlCommandJsonDataObject["groups"][i]["nonExistentSamples"] = [];
-	}
-
-	var cBioportalFormData = new FormData();
-	cBioportalFormData.append("DATA",JSON.stringify(curlCommandJsonDataObject));
+	var studyID;
+	let cbioportalCancerDictionaryPromise = [];
+	var cbioportalCancerDictionaryFormData = new FormData();
+	cbioportalCancerDictionaryFormData.append("DATA",props.cancer);
 	axios({
-	    method: "post",
-	    url: (targeturl.concat("/backend/cbioportal.php")),
-	    data: cBioportalFormData,
-	    headers: { "Content-Type": "multipart/form-data" },
+		    method: "post",
+		    url: (targeturl.concat("/backend/cBioportalTranslateStudies.php")),
+		    data: cbioportalCancerDictionaryFormData,
+		    headers: { "Content-Type": "multipart/form-data" },
 	})
-	    .then(function (response) {
-	      var returned_uuid = response["data"]["id"];
-	      //uuid in this case is a comparison id returned by the curl command. It gives us the correct destination for our survival plot.
-	      goToCBio(returned_uuid);
-	})
+	.then(function (res) {
+		studyID = res["data"];
+		let curlCommandJsonDataObject = {"groups": [], "origin": [studyID]};
+
+		for(let i in cBioportalJsonData)
+		{
+			curlCommandJsonDataObject["groups"][i] = {};
+			curlCommandJsonDataObject["groups"][i]["name"] = props.label[i];
+			curlCommandJsonDataObject["groups"][i]["description"] = "";
+			curlCommandJsonDataObject["groups"][i]["studies"] = [];
+			curlCommandJsonDataObject["groups"][i]["studies"][0] = {};
+			curlCommandJsonDataObject["groups"][i]["studies"][0]["id"] = studyID;
+			curlCommandJsonDataObject["groups"][i]["studies"][0]["samples"] = cBioportalJsonData[i];
+			curlCommandJsonDataObject["groups"][i]["studies"][0]["patients"] = cBioportalJsonData[i];
+			curlCommandJsonDataObject["groups"][i]["origin"] = [];
+			curlCommandJsonDataObject["groups"][i]["origin"][0] = studyID;
+			curlCommandJsonDataObject["groups"][i]["uid"] = "62665b890934121b56df06b5".concat(i.toString());
+			curlCommandJsonDataObject["groups"][i]["isSharedGroup"] = false;
+			curlCommandJsonDataObject["groups"][i]["nonExistentSamples"] = [];
+		}
+
+		var cBioportalFormData = new FormData();
+		cBioportalFormData.append("DATA",JSON.stringify(curlCommandJsonDataObject));
+		axios({
+		    method: "post",
+		    url: (targeturl.concat("/backend/cbioportal.php")),
+		    data: cBioportalFormData,
+		    headers: { "Content-Type": "multipart/form-data" },
+		})
+		    .then(function (response) {
+		      var returned_uuid = response["data"]["id"];
+		      //uuid in this case is a comparison id returned by the curl command. It gives us the correct destination for our survival plot.
+		      goToCBio(returned_uuid);
+		})
+    });
+
 }
 
 //This component facilitates the retrieval of information from cBioportal (using our selected sample names) to complete a URL linkout for visualizing
